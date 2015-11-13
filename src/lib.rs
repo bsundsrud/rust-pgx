095feddac7ea763dc84e69b7e75432a2d5a84573
Benn Sundsrud <benn.sundsrud@code42.com>
2015-11-13 14:43:34 -0600

    Clean up library, add macro, add example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,58 @@
-#[test]
-fn it_works() {
+extern crate postgres;
+
+use postgres::stmt::Statement;
+use postgres::types::ToSql;
+use postgres::rows::{Row, IntoIter};
+use postgres::error::Error;
+
+use std::marker::PhantomData;
+use std::iter::Iterator;
+
+pub trait FromRow {
+	fn from_row<'a>(row: Row<'a>) -> Self;
+}
+
+pub struct RowIterator<'a, T> {
+    _marker: PhantomData<T>,
+    iter: IntoIter<'a>
+}
+
+impl<'a, T> Iterator for RowIterator<'a, T>
+where T: FromRow {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+		match self.iter.next() {
+            Some(a) => Some(T::from_row(a)),
+            _ => None,
+        }
+    }
+}
+
+pub fn queryx<'a, T>(stmt: &'a Statement, args: &[&ToSql]) -> Result<RowIterator<'a, T>, Error>
+		where T: FromRow {
+	let rows = try!(stmt.query(args));
+	let iter = rows.into_iter();
+	Ok(RowIterator {
+	    iter: iter,
+	    _marker: PhantomData
+	})
+}
+
+#[macro_export]
+macro_rules! pgx_row {
+	(
+		$type_name: ident,
+		$( $field: ident : $idx: expr),*
+	) => {
+		use postgres::rows::Row;
+		impl FromRow for $type_name {
+			fn from_row<'a>(row: Row<'a>) -> $type_name {
+		        $type_name {
+		            $(
+		            	$field: row.get($idx),
+		            )*
+		        }
+		    }
+		}
+	}
 }
---
+++ tests/lib.rs
@@ -0,0 +1,54 @@
+extern crate postgres;
+#[macro_use]
+extern crate pgx;
+
+use postgres::{Connection, SslMode};
+use pgx::{FromRow, queryx};
+
+#[derive(Debug)]
+struct Person {
+    id :i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+pgx_row!{Person, id: 0, name: 1, data: 2}
+
+fn insert_data(conn: &Connection) {
+    conn.execute("CREATE TABLE person (
+                    id              SERIAL PRIMARY KEY,
+                    name            VARCHAR NOT NULL,
+                    data            BYTEA
+                  )", &[]).unwrap();
+    let him = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    let me = Person {
+        id: 0,
+        name: "Benn".to_string(),
+        data: None,
+    };
+    conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
+                 &[&me.name, &me.data]).unwrap();
+    conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
+                 &[&him.name, &him.data]).unwrap();
+}
+
+fn drop_data(conn: &Connection) {
+    conn.execute("DROP TABLE person;", &[]).unwrap();
+}
+#[test]
+fn run_test() {
+    let conn = Connection::connect("postgres://postgres:postgres@localhost/pgx", &SslMode::None).unwrap();
+    
+    insert_data(&conn);
+
+    let stmt = conn.prepare("SELECT id, name, data FROM person").unwrap();
+    let people = queryx::<Person>(&stmt, &[]).unwrap().collect::<Vec<Person>>();
+    assert_eq!(people[0].name, "Benn");
+    assert_eq!(people[1].name, "Steven");
+
+    drop_data(&conn);
+}
